cde95763ad5ffa916e4c2b56602cb0fc7fa4df6e
Daniel Donenfeld <danielbd@mit.edu>
2021-08-14 00:29:21 -0400

    Update brighten script for lanka

--- scripts/brighten.sh
+++ scripts/brighten.sh
@@ -11,9 +11,12 @@ out=/data/scratch/danielbd/taco-compression-benchmarks/out/brighten/
 validation=/data/scratch/danielbd/taco-compression-benchmarks/out/brighten/validation/
 mkdir -p "$out"
 mkdir -p "$validation"
+mkdir -p "$validation/character1/"
+mkdir -p "$validation/character2/"
+mkdir -p "$validation/merrie_melodies"
 
-LANKA=ON IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/character1" IMAGE_START=1602 IMAGE_END=2201 CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/character1.log
+LANKA=ON VALIDATION_OUTPUT_PATH="$validation/character1/" IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/character1/" IMAGE_START=1602 IMAGE_END=2201 CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/character1.log
 
-LANKA=ON IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/character2" IMAGE_START=3023 IMAGE_END=3622  CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/character2.log
+LANKA=ON VALIDATION_OUTPUT_PATH="$validation/character2/" IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/character2/" IMAGE_START=3023 IMAGE_END=3622  CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/character2.log
 
-LANKA=ON IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/merrie_melodies" IMAGE_START=1 IMAGE_END=600 CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/merrie_melodies.log
+LANKA=ON VALIDATION_OUTPUT_PATH="$validation/merrie_melodies/" IMAGE_FOLDER="/data/scratch/danielbd/clips/bs/merrie_melodies/" IMAGE_START=1 IMAGE_END=600 CACHE_KERNELS=0 make taco-bench BENCHES="brighten" > $out/merrie_melodies.log
