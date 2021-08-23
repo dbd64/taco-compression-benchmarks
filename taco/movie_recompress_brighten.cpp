bcd3521048b026a0b81cda207a7891d862f8fdce
Daniel Donenfeld <danieldonenfeld@gmail.com>
2021-08-23 18:52:20 -0400

    Update lz77 recompression bench

--- taco/movie_recompress_brighten.cpp
+++ taco/movie_recompress_brighten.cpp
@@ -25,104 +25,126 @@ using namespace taco;
 #define MAX_DIST 65535
 
 namespace {
-int computeHash(const std::vector<uint8_t>& in, int in_idx){
+__attribute__((always_inline))
+int computeHash(const uint8_t* in, const int in_idx){
   return ( ( in[ in_idx ] & 0xFF ) << 8 ) |
          ( ( in[ in_idx + 1 ] & 0xFF ) ^ ( in[ in_idx + 2 ] & 0xFF ) );
 }
 
+__attribute__((always_inline))
 std::pair<int,int> find_match(const int in_idx, const int out_idx, const int start_idx,
-                              const std::vector<uint8_t>& in, const std::vector<uint8_t>& out) {
+                              const uint8_t* in, int insize, const uint8_t* out, int outsize) {
   int len = 1;
   int off = out_idx - start_idx;
-  if( off > 0 && off < 65536 && start_idx >= 0 && start_idx < out.size() &&
+  if( off > 0 && off < 65536 && start_idx >= 0 && start_idx < outsize &&
       out[ start_idx ] == in[ in_idx ] ) {
-    while( in_idx + len < in.size() && len <= MAX_RUN && 
-           out[ start_idx + (len % off) ] == in[ in_idx + len ] ) {
+    // while( in_idx + len < insize && len <= MAX_RUN && 
+    //        out[ start_idx + (len % off) ] == in[ in_idx + len ] ) {
+    //   len++;
+    // }
+    int out_len = len;
+    while( in_idx + len < insize && len <= MAX_RUN && 
+           out[ start_idx + out_len ] == in[ in_idx + len ] ) {
       len++;
+      out_len++;
+      if (out_len == off) out_len = 0;
     }
   }
   return {len, off};
 }
 
-union short_byte {
-    uint16_t val;
-    uint8_t bytes[2];
-};
-
-void push_uint16(std::vector<uint8_t>& out, uint16_t value){
-  short_byte store{value};
-  out.push_back(store.bytes[0]);
-  out.push_back(store.bytes[1]);
+__attribute__((always_inline))
+void push_uint16(uint8_t* out, int& outsize, int& outcap, uint16_t value){
+  if (outsize >= outcap){
+    outcap *= 2;
+    out = (uint8_t*)realloc((void*)out, outcap);
+  }
+  uint8_t bytes[sizeof(value)];
+  memcpy(bytes, &value, sizeof(value));
+  out[outsize+0] = bytes[0];
+  out[outsize+1] = bytes[1];
+  outsize+=2;
 }
 
-void set_uint16(std::vector<uint8_t>& out, int index, uint16_t value){
-  short_byte store{value};
-  out[index + 0] = store.bytes[0];
-  out[index + 1] = store.bytes[1];
+__attribute__((always_inline))
+void set_uint16(uint8_t* out, int& outsize, int& outcap, const int index, uint16_t value){
+  uint8_t bytes[sizeof(value)];
+  memcpy(bytes, &value, sizeof(value));
+
+  out[index + 0] = bytes[0];
+  out[index + 1] = bytes[1];
 }
 
-uint16_t load_uint16(std::vector<uint8_t>& out, int index){
-  short_byte store;
-  store.bytes[0] = out[index];
-  store.bytes[1] = out[index+1];
-  return store.val;
+__attribute__((always_inline))
+uint16_t load_uint16(const uint8_t* out, int& outsize, int& outcap, const int index){
+  uint16_t val;
+  memcpy(&val, &out[index], sizeof(val));
+  return val;
 }
 
-void encode_lz77_(const std::vector<uint8_t>& in, std::vector<uint8_t> out) {
+__attribute__((noinline))
+void encode_lz77_(const uint8_t* in, const int insize, uint8_t*& out, int& outsize) {
   int in_idx = 0;
   int count_idx = -1;
-  std::vector<int> hash(65536);
+  int count = 0;
+  // std::vector<int> hash(65536);
+  int outcap = 1048576;
+  out = (uint8_t*) malloc(outcap);
+  outsize = 0;
 
-  while( in_idx < in.size() ) {
+  while( in_idx < insize ) {
     int len = 1;
     int off = 0;
-    if( in_idx + 2 < in.size() ) {
-      auto rle = find_match(in_idx, out.size(), out.size() - 1, in, out);
-      auto pixelRle = find_match(in_idx, out.size(), out.size() - 3, in, out);
-      auto hashCheck = find_match(in_idx, out.size(), hash[ computeHash(in, in_idx) ], in, out);
-      len = std::max({rle.first, pixelRle.first, hashCheck.first});
-      if (rle.first == len){
-        off = rle.second;
-      } else if (pixelRle.first == len) {
-        off = pixelRle.second;
-      } else {
-        off = hashCheck.second;
-      }
+    if( in_idx + 2 < insize ) {
+      auto pixelRle = find_match(in_idx, outsize, outsize - 3, in, insize, out, outsize);
+      len = pixelRle.first;
+      off = pixelRle.second;
     }
     if( len >= 4 ) {
-      // if( len > MAX_LIT ) {
-      //   len = MAX_LIT;
-      // }
-      push_uint16(out, 32768 | len);
-      push_uint16(out, off);
-      count_idx = -1;
-      hash[computeHash(out, out.size()-4)] = out.size()-4;
-      hash[computeHash(out, out.size()-3)] = out.size()-3;
+      push_uint16(out, outsize, outcap, 32768 | len);
+      push_uint16(out, outsize, outcap, off);
+      if (count_idx != -1){
+        set_uint16(out, outsize, outcap, count_idx, count);
+        count_idx = -1;
+      }
+      // hash[computeHash(out, outsize-4)] = outsize-4;
+      // hash[computeHash(out, outsize-3)] = outsize-3;
       in_idx+=len;
     } else {
       while( len ) {
-        if (count_idx != -1 && load_uint16(out, count_idx)==MAX_LIT){
-          count_idx = -1;
-        }
         if (count_idx == -1) {
-          count_idx = out.size();
-          push_uint16(out, 0);
+          count_idx = outsize;
+          count = 0;
+          push_uint16(out, outsize, outcap, 0);
         }
         
-        int old_cout = load_uint16(out, count_idx);
+        int old_count = count; //load_uint16(out, outsize, outcap, count_idx);
+        if (old_count == MAX_LIT){
+          set_uint16(out, outsize, outcap, count_idx, old_count);
+          count_idx = outsize;
+          push_uint16(out, outsize, outcap, 0);
+        }
 
-        int new_count = len+old_cout > MAX_LIT ? MAX_LIT : len+old_cout;
-        int max = new_count - old_cout;
+        int new_count = len+old_count >= MAX_LIT ? MAX_LIT : len+old_count;
+        int max = new_count - old_count;
+
         len -= max;
-        set_uint16(out, count_idx, new_count);
-        if (count_idx+2<out.size()) {
-          hash[computeHash(out, count_idx)] = count_idx;
+        //set_uint16(out, outsize, outcap, count_idx, new_count);
+        count = new_count;
+        // if (count_idx+2<outsize) {
+        //   hash[computeHash(out, count_idx)] = count_idx;
+        // }
+        int outindex = outsize;
+        outsize+=max;
+        if (outsize >= outcap){
+          outcap = outcap*2;
+          out = (uint8_t*)realloc((void*)out, outcap);
         }
         while( max ) {
-          out.push_back(in[ in_idx++ ]);
-          if(out.size() > 2) {
-            hash[computeHash(out, out.size()-3)] = out.size()-3;
-          }
+          out[outindex++] = in[ in_idx++ ];
+          // if(outsize > 2) {
+          //   hash[computeHash(out, outsize-3)] = outsize-3;
+          // }
           max--;
         }
       }
@@ -225,14 +247,17 @@ void movie_compress_bench_brighten(){
     out.compute();
 
     auto denseVals = out.getStorage().getValues();
-    std::vector<uint8_t> vals((uint8_t*)denseVals.getData(), (uint8_t*)denseVals.getData() + denseVals.getSize());
+    // std::vector<uint8_t> vals((uint8_t*)denseVals.getData(), (uint8_t*)denseVals.getData() + denseVals.getSize());
+    int insize = denseVals.getSize();
+    uint8_t* in  = (uint8_t*)denseVals.getData();
+    std::cout << "in size: " << insize << std::endl;
     if (kind == Kind::LZ77){
       outputFile << index << "," << bench_kind << ",";
-      std::vector<uint8_t> reencoded;
-      reencoded.reserve(1048576);
+      uint8_t* out = 0;
+      int outsize = 0;
       TOOL_BENCHMARK_REPEAT({
-          encode_lz77_(vals, reencoded);
-          benchmark::DoNotOptimize(reencoded);
+          encode_lz77_(in, insize, out, outsize);
+          benchmark::DoNotOptimize(out);
       }, "Compute", repetitions, outputFile);
     }
   }
